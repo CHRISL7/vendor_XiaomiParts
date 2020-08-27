b3ea777392334d9184ab04190e654f2b48756c89
ppooopppooo <ppooopppooo@qq.com>
2020-08-27 16:03:15 +0800

    Dirac: Add files permission for dirac
    * corrected and revised by @chrisw444
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- xiaomiparts.mk
+++ xiaomiparts.mk
@@ -1,5 +1,13 @@
 VENDOR_PATH := vendor/XiaomiParts
 
+# Files Dirac
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/etc/permissions/audiosphere.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/audiosphere.xml \
+    $(VENDOR_PATH)/framework/audiosphere.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/audiosphere.jar \
+    $(VENDOR_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    $(VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
+    $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config
+
 # XiaomiParts
 PRODUCT_SOONG_NAMESPACES += \
    vendor/XiaomiParts
@@ -12,3 +20,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
    $(VENDOR_PATH)/overlay
+
+# Sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
