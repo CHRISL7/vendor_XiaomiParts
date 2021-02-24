0fa08487f62076b00ff70bcda7957c61ac8ae47c
Omar Hamad <etahamad@icloud.com>
2021-02-24 17:25:36 +0200

    Dirac: Add missed Files necessary

--- xiaomiparts.mk
+++ xiaomiparts.mk
@@ -6,7 +6,8 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/framework/audiosphere.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/audiosphere.jar \
     $(VENDOR_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
     $(VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
-    $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config
+    $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    $(VENDOR_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
 
 # XiaomiParts
 PRODUCT_SOONG_NAMESPACES += \
