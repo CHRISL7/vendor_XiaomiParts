74ee8e395df21443cb86e89b4d7cc9601c444a84
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-04-02 15:25:53 -0300

    Dirac: import audio.scd & diracvdd.bin from MIUI V12.0.1.0.RCOCNXM LAVENDER
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- xiaomiparts.mk
+++ xiaomiparts.mk
@@ -8,6 +8,7 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
     $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
     $(VENDOR_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin \
+    $(VENDOR_PATH)/vendor/etc/audio.scd:$(TARGET_COPY_OUT_VENDOR)/etc/audio.scd \
     $(VENDOR_PATH)/vendor/proprietary/vendor/etc/drc/drc_cfg_5.1.txt:$(TARGET_COPY_OUT_VENDOR)/etc/drc/drc_cfg_5.1.txt \
     $(VENDOR_PATH)/vendor/proprietary/vendor/etc/drc/drc_cfg_AZ.txt:$(TARGET_COPY_OUT_VENDOR)/etc/drc/drc_cfg_AZ.txt \
     $(VENDOR_PATH)/vendor/proprietary/vendor/etc/surround_sound_3mic/surround_sound_rec_5.1.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_3mic/surround_sound_rec_5.1.cfg \
