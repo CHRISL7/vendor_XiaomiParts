ElectroPerf/android_packages_apps_ZenParts

[SQUASH] Import ZenParts v3.0
- ZenParts v3.0 is a squashed import of All Available Xiaomi Parts Versions, Older ZenParts Versions and DeviceSettings
  in a single commit.
- UI Redesigns are also there.

Features:-
•Vibration Controll (System, Calls And Notifications)
•Display Color Calibration
•Grayscale & KCAL Presets
•Spectrum Profiles
•Dirac/Asus Sound Enhancer
•Headphone Gain
•Microphone Gain
•Earpiece Gain
•Speaker Gain
•Log Tile
•FPS Info
•AsusDoze
•Touch Boost
•CPU Boost
•GPU Boost
•MSM Thermal Control
•LKM Profiles
•TCP Congestion Algorithm
•SELinux Switch
•Backlight Dimmer
•Much More

Refrenced from:- https://github.com/xiaomi-sdm660/android_device_xiaomi_sdm660-common.git

Co-Authored-by: Sagarrokade006 <sagarrokade006@gmail.com>
Co-Authored-by: Kingsman44 <shivan.0972@gmail.com>
Co-Authored-by: TogoFire <italomellopereira@gmail.com>
Co-Authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
Co-Authored-by: Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
Co-Authored-by: smokey18 <kmboplays@gmail.com>
Co-Authored-by: Saurabh Charde <saurabhchardereal@gmail.com>
Co-Authored-by: SonalSingh18 <SonalSingh18@users.noreply.github.com>
Co-authored-by: Modit Jain <moditjain121@gmail.com>
Co-authored-by: Pranav Kapoor <pranavkapoor001@gmail.com>
Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>

src/com/asus/zenparts/BacklightDimmer.java
@@ -1,0 +1,65 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.asus.zenparts;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.asus.zenparts.R;
+
+public class BacklightDimmer implements OnPreferenceChangeListener {
+
+    private Context mContext;
+
+    public BacklightDimmer(Context context) {
+        mContext = context;
+    }
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(DeviceSettings.BACKLIGHT_DIMMER_PATH)) {
+            return DeviceSettings.BACKLIGHT_DIMMER_PATH;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFilesValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {
+            case DeviceSettings.PREF_BACKLIGHT_DIMMER:
+                FileUtils.setValue(DeviceSettings.BACKLIGHT_DIMMER_PATH, (boolean) value);
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+}

src/com/asus/zenparts/SuTask.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.asus.zenparts;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.asus.zenparts.DeviceSettings;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, R.string.cannot_get_su,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}

src/com/asus/zenparts/Touchboost.java
@@ -1,0 +1,65 @@
+/*
+* Copyright (C) 2020 ronaxdevil <pratabidya.007@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.asus.zenparts;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.asus.zenparts.R;
+
+public class Touchboost implements OnPreferenceChangeListener {
+
+    private Context mContext;
+
+    public Touchboost(Context context) {
+        mContext = context;
+    }
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(DeviceSettings.MSM_TOUCHBOOST_PATH)) {
+            return DeviceSettings.MSM_TOUCHBOOST_PATH;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {
+            case DeviceSettings.PREF_MSM_TOUCHBOOST:
+                FileUtils.setValue(DeviceSettings.MSM_TOUCHBOOST_PATH, (boolean) value);
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+}

CHRISL7/vendor_XiaomiParts

src/com/asus/zenparts/BacklightDimmer.java
@@ -1,0 +1,65 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.asus.zenparts;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.asus.zenparts.DeviceSettings;
+
+public class BacklightDimmer implements OnPreferenceChangeListener {
+
+    private Context mContext;
+
+    public BacklightDimmer(Context context) {
+        mContext = context;
+    }
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(DeviceSettings.BACKLIGHT_DIMMER_PATH)) {
+            return DeviceSettings.BACKLIGHT_DIMMER_PATH;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFilesValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {
+            case DeviceSettings.PREF_BACKLIGHT_DIMMER:
+                FileUtils.setValue(DeviceSettings.BACKLIGHT_DIMMER_PATH, (boolean) value);
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+}

src/com/asus/zenparts/SuTask.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.asus.zenparts;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.asus.zenparts.R;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, R.string.cannot_get_su,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}

src/com/asus/zenparts/Touchboost.java
@@ -1,0 +1,65 @@
+/*
+* Copyright (C) 2020 ronaxdevil <pratabidya.007@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.asus.zenparts;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.asus.zenparts.DeviceSettings;
+
+public class Touchboost implements OnPreferenceChangeListener {
+
+    private Context mContext;
+
+    public Touchboost(Context context) {
+        mContext = context;
+    }
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(DeviceSettings.MSM_TOUCHBOOST_PATH)) {
+            return DeviceSettings.MSM_TOUCHBOOST_PATH;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {
+            case DeviceSettings.PREF_MSM_TOUCHBOOST:
+                FileUtils.setValue(DeviceSettings.MSM_TOUCHBOOST_PATH, (boolean) value);
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+}