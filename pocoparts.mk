4c1d06c3432149003b6bff25f2d7d13647e552d6
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-09-08 01:06:58 -0300

    Parts: add init permissions e sepolicys the necessary
    Sepolicy:
     * set sm6150 plataform too
     * for fps info
     * for kcal
    
    referring commits:
     * https://github.com/ChrisW444/vendor_PocoParts/commit/171094b25a8bd03386285331721c336a74af2b17
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- pocoparts.mk
+++ pocoparts.mk
@@ -6,3 +6,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_PACKAGES += \
     PocoParts
+
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/init/init.pocoparts.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.pocoparts.rc
+
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+TARGET_SEPOLICY_DIR := msmsteppe
