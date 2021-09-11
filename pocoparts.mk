bb7988f65bf1c6fc90999204a8a08c7b33ed5e9a
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-09-11 19:13:18 -0300

    Parts: Add all permissions for Dirac
    * props
    * sepolicys
    * vendor blobs
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- pocoparts.mk
+++ pocoparts.mk
@@ -1,5 +1,12 @@
 VENDOR_PATH := vendor/PocoParts
 
+# Files Dirac
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    $(VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
+    $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    $(VENDOR_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
+
 # PocoParts
 PRODUCT_SOONG_NAMESPACES += \
     vendor/PocoParts
@@ -14,6 +21,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.lcd.cabc_mode=1
+
+# Properties
+TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
